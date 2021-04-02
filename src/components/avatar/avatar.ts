2648d08eaa9198d062acef029fda1d1855ae04ab
niu-grandpa <2864103063@qq.com>
2021-04-02 11:15:31 +0800

    重构

--- src/components/avatar/avatar.ts
+++ src/components/avatar/avatar.ts
@@ -1,8 +1,22 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { $el, bind, getStrTypeAttr, removeAttrs, setCss, setHtml } from '../../dom-utils';
 import { warn } from '../../mixins';
-import { $el, createElem, removeAttrs, setCss, setHtml } from '../../dom-utils';
+import { type, validComps } from '../../utils';
 import PREFIX from '../prefix';
 
-class Avatar {
+interface Config {
+    config(
+        el: string
+    ): {
+        events({ onError }: AvatarEvent): void;
+    };
+}
+
+interface AvatarEvent {
+    onError: (event: Event) => void;
+}
+
+class Avatar implements Config {
     readonly VERSION: string;
     readonly COMPONENTS: NodeListOf<Element>;
 
@@ -12,130 +26,114 @@ class Avatar {
         this._create(this.COMPONENTS);
     }
 
+    public config(
+        el: string
+    ): {
+        events({ onError }: AvatarEvent): void;
+    } {
+        const target = $el(el) as HTMLElement;
+
+        validComps(target, 'avatar');
+
+        const AvatarImage = target.querySelector('img');
+
+        return {
+            events({ onError }: AvatarEvent) {
+                if (!AvatarImage) {
+                    warn(
+                        `Unable to add an event where the image failed to load for the current avatar --> "${el}"`
+                    );
+                    return;
+                }
+
+                bind(AvatarImage, 'error', (event: Event) => onError && type.isFn(onError, event));
+            }
+        };
+    }
+
     private _create(COMPONENTS: NodeListOf<Element>): void {
         COMPONENTS.forEach((node) => {
-            this._setIcon(node);
-            this._setImage(node);
-            this._setSize(node);
-            this._setCustomContent(node);
+            const { icon, src, alt, size } = this._attrs(node);
 
-            removeAttrs(node, ['icon', 'src', 'custom-icon']);
+            this._setSize(node, size);
+            this._setIcon(node, icon);
+            this._setImage(node, src, alt);
+            this._setText(node, icon, src);
+
+            removeAttrs(node, ['icon', 'src', 'alt']);
         });
     }
 
-    private _setIcon(node: Element): void {
-        const hasIcon = this._getIcon(node);
-        const customIcon = this._getCustmIcon(node);
+    private _setSize(node: Element, size: string): void {
+        if (!size) return;
 
-        if (!hasIcon) return;
+        const _size = Number(size);
 
-        if (hasIcon || customIcon) {
-            setHtml(node, '');
-            node.classList.add(`${PREFIX.avatar}-icon`);
-        }
+        if (!_size) return;
 
-        if (customIcon) {
-            setHtml(node, customIcon);
-        } else {
-            const AvatarIcon = createElem('i');
-            AvatarIcon.className = `rab-icon rab-icon-${hasIcon}`;
-            node.appendChild(AvatarIcon);
-        }
+        setCss(node, 'width', `${_size}px`);
+        setCss(node, 'height', `${_size}px`);
+        setCss(node, 'fontSize', `${_size / 2}px`);
+
+        node.removeAttribute('size');
     }
 
-    private _setImage(node: Element): void {
-        const hasSrc = this._getSrc(node);
+    private _setIcon(node: Element, icon: string): void {
+        if (!icon) return;
 
-        if (!hasSrc) return;
+        node.classList.add(`${PREFIX.avatar}-icon`);
 
-        const AvatarImage = createElem('img');
+        const AvatarIcon = `<i class="${PREFIX.icon} ${PREFIX.icon}-${icon}"></i>`;
+        setHtml(node, AvatarIcon);
+    }
 
-        // @ts-ignore
-        AvatarImage.src = hasSrc;
-
-        setHtml(node, '');
+    private _setImage(node: Element, src: string, alt: string): void {
+        if (!src) return;
 
         node.classList.add(`${PREFIX.avatar}-image`);
 
-        node.appendChild(AvatarImage);
+        const AvatarImage = `<img src="${src}" alt="${alt}" />`;
+        setHtml(node, AvatarImage);
     }
 
-    // 自定义头像内容
-    private _setCustomContent(node: Element): void {
-        if (node.getAttribute('rb-icon') || node.getAttribute('rb-src')) return;
+    private _setText(node: Element, icon: string, src: string): void {
+        if (icon || src) return;
 
-        if (setHtml(node)) {
-            const AvatarStrBox = createElem('span');
+        if (!setHtml(node)) return;
 
-            AvatarStrBox.className = `${PREFIX.avatar}-string`;
+        const text = setHtml(node);
 
-            setHtml(AvatarStrBox, node.innerHTML);
-            setHtml(node, '');
+        const AvatarText = `<span class="${PREFIX.avatar}-string">${text}</span>`;
 
-            node.appendChild(AvatarStrBox);
+        setHtml(node, AvatarText);
 
-            this._setScale(node, AvatarStrBox);
-        }
+        this._setScale(node);
     }
 
-    // 使文本容器大写适应头像容器并且不超出范围
-    private _setScale(node: Element, children: HTMLSpanElement) {
-        const avatarWidth: number = node.getBoundingClientRect().width;
-        const childrenWidth: number = children.offsetWidth;
+    // 防止字符型头像的字体溢出边界
+    private _setScale(node: Element) {
+        const children = node.querySelector(`.${PREFIX.avatar}-string`)! as HTMLElement;
+
+        if (!children) return;
+
+        const avatarWidth = node.getBoundingClientRect().width;
+        const childrenWidth = children.offsetWidth;
 
         if (avatarWidth - 8 < childrenWidth) {
             const newScale = `scale(${(avatarWidth - 8) / childrenWidth}) translateX(-50%)`;
-            setCss(children, 'transform', `scale(${newScale}) translateX(-50%)`);
+            setCss(children, 'transform', `${newScale}`);
         } else {
             setCss(children, 'transform', 'scale(1) translateX(-50%)');
         }
     }
 
-    // 设置头像尺寸
-    private _setSize(node: any): void {
-        const size = this._getSize(node);
-
-        if (!size) return;
-
-        setCss(node, 'width', `${size}px`);
-        setCss(node, 'height', `${size}px`);
-        setCss(node, 'lineHeight', `${size}px`);
-
-        // 如果设置的尺寸超过40，且在有图标的情况下将其字体大写设为头像尺寸的一半
-        if (size >= 40) {
-            if (node.querySelector('.rab-icon')) {
-                const newFontSize = `${Math.floor(size / 2)}px`;
-                setCss(node, 'fontSize', newFontSize);
-            }
-        }
-    }
-
-    private _getIcon(node: Element): string {
-        return node.getAttribute('rb-icon') || '';
-    }
-
-    private _getCustmIcon(node: Element): string {
-        return node.getAttribute('rb-custom-icon') || '';
-    }
-
-    private _getSrc(node: Element): string {
-        return node.getAttribute('rb-src') || '';
-    }
-
-    private _getSize(node: Element): number | undefined {
-        const size = node.getAttribute('rb-size') || '';
-
-        // 不获取这两个
-        if (size === 'large' || size === 'small') return;
-
-        // 尺寸设置为数值，则将字符串数值转换为数字
-        const toNum = Number(size);
-
-        if (toNum) return toNum;
-        // 打印错误不包括设置了数值为0
-        else if (toNum !== 0) {
-            warn(`You have set an invalid size value for the Avatar component --> "${size}"`);
-        }
+    private _attrs(node: Element) {
+        return {
+            icon: getStrTypeAttr(node, 'icon', ''),
+            src: getStrTypeAttr(node, 'src', ''),
+            alt: getStrTypeAttr(node, 'alt', ''),
+            size: getStrTypeAttr(node, 'size', '')
+        };
     }
 }
 
