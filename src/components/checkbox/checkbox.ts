33dafb1396f75ae53d28f46c1a3b2b4cdb285765
niu-grandpa <2864103063@qq.com>
2021-04-02 20:09:37 +0800

    更新

--- src/components/checkbox/checkbox.ts
+++ src/components/checkbox/checkbox.ts
@@ -1,4 +1,11 @@
-import { $el } from '../../dom-utils';
+import {
+    $el,
+    getArrTypeAttr,
+    getBooleanTypeAttr,
+    getStrTypeAttr,
+    removeAttrs,
+    setHtml
+} from '../../dom-utils';
 import PREFIX from '../prefix';
 
 class Checkbox {
@@ -13,8 +20,42 @@ class Checkbox {
 
     private _create(COMPONENTS: NodeListOf<Element>) {
         COMPONENTS.forEach((node) => {
-            //
+            const { name, checked } = this._attrs(node);
+
+            this._setChecked(node, checked);
+            this._setMainTemplate(node, name);
+
+            removeAttrs(node, ['name', 'checked']);
         });
+    }
+
+    private _setChecked(node: Element, checked: boolean): void {
+        if (!checked) return;
+        node.classList.add(`${PREFIX.checkbox}-checked`);
+    }
+
+    private _setMainTemplate(node: Element, name: string) {
+        const content = setHtml(node);
+
+        const template = `
+         <span class="${PREFIX.checkbox}">
+            <span class="${PREFIX.checkbox}-inner"></span>
+                <input type="checkbox" class="${PREFIX.checkbox}-input" name="${name}">
+            </span>
+         <span>${content}</span>
+        `;
+
+        setHtml(node, template);
+    }
+
+    private _attrs(node: Element) {
+        return {
+            name: getStrTypeAttr(node, 'name', ''),
+            label: getStrTypeAttr(node, 'label', ''),
+            value: getArrTypeAttr(node, 'value'),
+            checked: getBooleanTypeAttr(node, 'checked'),
+            indeterminate: getBooleanTypeAttr(node, 'indeterminate')
+        };
     }
 }
 
