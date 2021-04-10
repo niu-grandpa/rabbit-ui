bb8c41237b1d04abfb226aa4e89a378aa0fb4a13
niu-grandpa <2864103063@qq.com>
2021-04-10 21:33:08 +0800

    搭建文档网站

---
+++ site/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'App',
+    components: {}
+});
+</script>
+
+<style lang="less">
+</style>
