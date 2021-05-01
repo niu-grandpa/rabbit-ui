1e6096ecc457aec253707c3f389ac7bb5c66c1cb
niu-grandpa <2864103063@qq.com>
2021-05-01 19:51:11 +0800

    更新

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 import styleImport from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: 'css/[name].[hash].css',
+                chunkFileNames: 'js/[name].[hash].js',
+                entryFileNames: 'js/[name].[hash].js'
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            pages: path.resolve(__dirname, 'src/pages')
+        }
+    },
     plugins: [
         vue(),
         styleImport({
