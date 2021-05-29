40bcc56117242609c547fe447f5d5b8bd70b20d0
niu-grandpa <2864103063@qq.com>
2021-05-29 17:28:19 +0800

    更新

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -32,7 +32,7 @@ module.exports = merge(common, {
         new HtmlWebpackPlugin({
             inject: true,
             filename: path.join(__dirname, '../examples/dist/index.html'),
-            template: path.join(__dirname, '../examples/affix/index.html')
+            template: path.join(__dirname, '../examples/circle/index.html')
         }),
         new FriendlyErrorsPlugin()
     ]
