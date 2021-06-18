54b3fb1f45278e492d57bf05c5725897f4d9432a
niu-grandpa <2864103063@qq.com>
2021-06-18 16:01:19 +0800

    修复在没有图标仅有文本的状态下，切换为loading状态并在状态结束后无法切换为原样 bug

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -23,7 +23,7 @@ class Button implements Config {
     readonly COMPONENTS: NodeListOf<Element>;
 
     constructor() {
-        this.VERSION = '1.1.0';
+        this.VERSION = '1.1.1';
         this.COMPONENTS = $el(`.${PREFIX.button}`, { all: true });
         this._getAllBtns(this.COMPONENTS);
     }
@@ -34,12 +34,11 @@ class Button implements Config {
         loading: boolean;
     } {
         const target = typeof el === 'string' ? $el(el) : el;
-
         validComps(target, 'button');
 
         return {
             get loading() {
-                return false;
+                return this.loading;
             },
             set loading(newVal) {
                 if (newVal && !type.isBol(newVal)) return;
@@ -51,17 +50,14 @@ class Button implements Config {
     private _getAllBtns(COMPONENTS: NodeListOf<Element>): void {
         COMPONENTS.forEach((node) => {
             const { icon, loading } = this._attrs(node);
-
             this._setIcon(node, icon);
             this._setLoading(node, true, loading);
-
             removeAttrs(node, ['icon', 'loading']);
         });
     }
 
     private _setIcon(node: Element, icon: string): void {
         if (!icon) return;
-
         if (node.innerHTML === '') {
             node.classList.add(`${PREFIX.button}-icon-only`);
             const ButtonIcon = `
@@ -81,29 +77,22 @@ class Button implements Config {
         const OriginalIcon = node.querySelector('.rab-icon');
         const LoadingIcon = createElem('i');
         LoadingIcon.className = `rab-load-loop ${PREFIX.icon} ${PREFIX.icon}-loading-solid`;
-
         if (loading) {
-            if (OriginalIcon) {
-                setCss(OriginalIcon, 'display', 'none');
-            }
-            if (node.innerHTML === '') {
-                node.classList.add(`${PREFIX.button}-icon-only`);
-            }
-
+            if (OriginalIcon) setCss(OriginalIcon, 'display', 'none');
+            if (node.innerHTML === '') node.classList.add(`${PREFIX.button}-icon-only`);
             node.classList.add(`${PREFIX.button}-loading`);
             node.prepend(LoadingIcon);
         } else {
             if (firstRender) return;
-
-            setCss(node.children[1], 'display', '');
-            node.firstElementChild?.remove();
-
-            if (node.classList.contains(`${PREFIX.button}-loading`)) {
+            // 2021.6.18
+            // v1.1.1 修复在没有图标仅有文本的状态下，切换为loading状态并在状态结束后无法切换为原样 bug
+            // setCss(node.children[1], 'display', '');
+            if (node.children[1]) setCss(node.children[1], 'display', '');
+            if (node.classList.contains(`${PREFIX.button}-loading`))
                 node.classList.remove(`${PREFIX.button}-loading`);
-            }
-            if (node.classList.contains(`${PREFIX.button}-icon-only`)) {
+            if (node.classList.contains(`${PREFIX.button}-icon-only`))
                 node.classList.remove(`${PREFIX.button}-icon-only`);
-            }
+            node.firstElementChild?.remove();
         }
     }
 
